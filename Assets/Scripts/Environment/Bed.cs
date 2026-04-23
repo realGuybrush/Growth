154f32c79f069acf454a689432c3cb55a06745cd
realGuybrush <TripleBaka@yandex.ru>
2026-04-23 17:23:18 +0200

    Added UI for screen darkening and sleep logic.

--- Assets/Scripts/Environment/Bed.cs
+++ Assets/Scripts/Environment/Bed.cs
@@ -1,21 +1,41 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class Bed : MonoBehaviour
 {
     [SerializeField]
-    private GameObject message;
+    private GameObject message, bedBlocker;
+
+    [SerializeField]
+    private Rigidbody2D player;
+
+    [SerializeField]
+    private float sleepTime = 4f;
     
     public event Action OnSleep = delegate { };
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //message.SetActive(true);
-        Sleep();
+        message.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        message.SetActive(false);
+        bedBlocker.SetActive(true);
     }
 
     public void Sleep()
     {
+        player.transform.position = transform.position;
+        StartCoroutine("DisablePlayer");
         OnSleep?.Invoke();
     }
 
+    private IEnumerator DisablePlayer()
+    {
+        player.constraints = RigidbodyConstraints2D.FreezeAll;
+        yield return new WaitForSeconds(sleepTime);
+        player.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
 }
---
+++ Assets/Scripts/UI/AnimatorSetBool.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class AnimatorSetBool : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    public void SetBool()
+    {
+        //Yes, it cannot be done via UI. Who could've known?
+        animator.SetBool("On", true);
+    }
+}
